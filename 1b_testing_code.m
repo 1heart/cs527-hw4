37a74e7ee97edf10ddd60b9ee36bb9665f34720c
Cody Lieu <cody.a.lieu@gmail.com>
2015-10-26 05:28:25 -0400

    Finished problem 1

--- 1b_testing_code.m
+++ 1b_testing_code.m
@@ -8,3 +8,11 @@ clf
 plot(x, f(x), 'b')
 hold on
 plot(x, y, 'r')
+xlabel('X')
+ylabel('Y')
+title('Two-Layer Neural Network with 64 Gains and Biases')
+legend('f(x) = exp(x) * sin(3 * pi * x)', 'Two-Layer Approximation', 'Location', 'northwest')
+
+% 1c
+% k = 21 and there were 64 gains and biases
+nw = 3k + 1
--- nn.m
+++ nn.m
@@ -3,8 +3,8 @@ function X = nn(X, net)
 for i = 1:size(net, 2)
     curNet = net(i);
     W = [curNet.gain curNet.bias];
-    xTilde = [X; ones(1, size(X, 2))];
-    a = W*xTilde;
+    X = [X; ones(1, size(X, 2))];
+    a = W*X;
     X = curNet.h(a);
 end
 
